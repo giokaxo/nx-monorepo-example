291342e031b46bcdbf82dbe98c0e11607a87629b
Giorgi Kakhoshvili <giorgi.kakhoshvili@rebilly.com>
2025-11-25 13:03:16 +0100

    feat(is-odd): Add `isOdd6`

--- test-monorepo/packages/is-odd/index.ts
+++ test-monorepo/packages/is-odd/index.ts
@@ -9,3 +9,5 @@ export const isOdd3 = (x: number) => !isEven(x);
 export const isOdd4 = (x: number) => x % 2 !== 0;
 
 export const isOdd5 = (x: number) => x % 2 !== 0;
+
+export const isOdd6 = (x: number) => x % 2 !== 0;
